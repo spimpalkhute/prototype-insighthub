ade5dac038d6efa8901c4762b2b8bf768247e501
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-01-15 12:34:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import streamlit as st
 import pandas as pd
+import datetime
+
 # Streamlit UI
 st.set_page_config(
     page_title="Prototype-Insighthub",
     page_icon="👋",
 )
 st.title("Explore the world of Indian Startups")
+st.divider()
+    st.markdown(
+        "<h3 style='text-align: center; color: black;'>InsightHub: Empowering Investment Decisions in India's Entrepreneurial Landscape </h1>",
+        unsafe_allow_html=True)
+    with open('./clock.time', 'r') as f:
+        last_updated_on = f.readlines()[0]
+    st.caption(last_updated_on)
+    st.markdown('')
+    st.image('img/tfinder_schema.png')
+    st.markdown('')
+    st.markdown('**Overview**')
 df = pd.DataFrame({
     'first column': ['', 'Start-up Founder', 'Venture Capitalist'],
     })
@@ -15,4 +28,4 @@ option = st.selectbox(
      df['first column'])
 
 'You selected: ', option
-
+    
