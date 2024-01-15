9a83274df488d1c5d6e3fee57cdef2ab00c44e4d
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-01-15 13:04:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,13 @@ option = st.selectbox(
 
 'You selected: ', option
 
-file_path = "Dataset_Unicorn.csv"  # Replace with your actual file path
-df = pd.read_csv(file_path, encoding='utf-8')
+csv_file_path = "Dataset_Unicorn.csv"  # Replace with your actual file path
+try:
+    # Try reading with utf-8 encoding
+    df = pd.read_csv(csv_file_path, encoding='utf-8')
+except UnicodeDecodeError:
+    # If utf-8 fails, try reading with latin-1 encoding
+    df = pd.read_csv(csv_file_path, encoding='latin-1', errors='replace')
 
 # Display the DataFrame
 st.write("Indian Unicorns:")
