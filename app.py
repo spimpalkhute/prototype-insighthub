4efb54f71997b69c224e57411c97220ba05990de
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-01-15 12:25:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+import streamlit as st
+import pandas as pd
 # Streamlit UI
 st.set_page_config(
     page_title="Prototype-Insighthub",
     page_icon="👋",
 )
 st.title("Explore the world of Indian Startups")
+df = pd.DataFrame({
+    'first column': ['Start-up Founder', 'Venture Capitalist'],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'You are:',
+     df['first column'])
+
+'You selected: ', option
+
