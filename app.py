8f19b00c732c80c3ba7d3535453608d1cd7936b1
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-01-15 12:35:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.divider()
 st.markdown(
         "<h3 style='text-align: center; color: black;'>InsightHub: Empowering Investment Decisions in India's Entrepreneurial Landscape </h1>",
         unsafe_allow_html=True)
- with open('./clock.time', 'r') as f:
+with open('./clock.time', 'r') as f:
     last_updated_on = f.readlines()[0]
     st.caption(last_updated_on)
     st.markdown('')
