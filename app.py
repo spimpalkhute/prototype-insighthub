a31a799beb7bc6c582a74e078d10ca7ac8290ae4
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-01-15 12:36:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,6 @@ st.divider()
 st.markdown(
         "<h3 style='text-align: center; color: black;'>InsightHub: Empowering Investment Decisions in India's Entrepreneurial Landscape </h1>",
         unsafe_allow_html=True)
-with open('./clock.time', 'r') as f:
-    last_updated_on = f.readlines()[0]
-    st.caption(last_updated_on)
-    st.markdown('')
-    st.image('img/tfinder_schema.png')
     st.markdown('')
     st.markdown('**Overview**')
 df = pd.DataFrame({
