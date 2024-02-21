97c3e91750f25f43114990c49ac02ba4431c42ae
issaumyya <70967068+issaumyya@users.noreply.github.com>
2024-02-21 15:01:04 +0530

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -43,14 +43,27 @@ with col1:
     st.plotly_chart(fig,use_container_width=True, height = 200)
 
 top_companies = df.nlargest(10, 'Operating Revenue (FY23)')
+top_companies1 = df.nlargest(10, 'Employee Benefit (FY23)')
 with col2:
-    fig1 = go.Figure()
-    fig1.add_trace(go.Bar(x=top_companies['Company Name'], y=top_companies['Operating Revenue (FY23)'],
+    choice = st.selectbox("Choose one:", ["Operating Revenue", "Employee Benefit", "Advertisement"])
+    if choice =="Operating Revenue":
+        fig1 = go.Figure()
+        fig1.add_trace(go.Bar(x=top_companies['Company Name'], y=top_companies['Operating Revenue (FY23)'],
                      name='Operating Revenue (FY23)'))
-    fig1.add_trace(go.Bar(x=top_companies['Company Name'], y=top_companies['Operating Revenue (FY22)'],
+        fig1.add_trace(go.Bar(x=top_companies['Company Name'], y=top_companies['Operating Revenue (FY22)'],
                      name='Operating Revenue (FY22)'))
-    fig1.update_layout(barmode='group',
+        fig1.update_layout(barmode='group',
                   title='Operating Revenue Comparison (FY23 vs FY22) for Top Companies',
                   xaxis_title='Company',
                   yaxis_title='Operating Revenue')
+    elif choice =="Employee Benefit":
+        fig1 = go.Figure()
+        fig1.add_trace(go.Bar(x=top_companies1['Company Name'], y=top_companies1['Employee Benefit (FY23)'],
+                     name='Employee Benefit (FY23)'))
+        fig1.add_trace(go.Bar(x=top_companies1['Company Name'], y=top_companies1['Employee Benefit (FY22)'],
+                     name='Employee Benefit (FY22)'))
+        fig1.update_layout(barmode='group',
+                  title='Employee Benefit Comparison (FY23 vs FY22) for Top Companies',
+                  xaxis_title='Company',
+                  yaxis_title='Employee Benefit')
     st.plotly_chart(fig1,use_container_width=True, height = 200)
